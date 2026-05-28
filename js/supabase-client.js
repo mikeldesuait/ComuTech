e24243768e33a38750daeeb5ee3c38e29e17d2f1
mikeldesuait <mikeldesuait@gmail.com>
2026-05-29 00:11:54 +0200

    Delete js directory

--- js/supabase-client.js
+++
@@ -1,4 +0,0 @@
-// js/supabase-client.js
-const SUPABASE_URL = "https://idbdkxhhqeuarcqcaweo.supabase.co";
-const SUPABASE_KEY = "sb_publishable_uNWQl8gDfrmrPWS_1uEVcg_gMeqjO2U";
-const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
