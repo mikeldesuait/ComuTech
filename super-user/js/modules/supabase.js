918d24cbdd360ed937ef037ed3d04ef42dfc204f
mikeldesuait <mikeldesuait@gmail.com>
2026-05-30 00:10:18 +0200

    Update supabase.js

--- super-user/js/modules/supabase.js
+++ super-user/js/modules/supabase.js
@@ -1,8 +1,6 @@
-// js/modules/supabase.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 const SUPABASE_URL = "https://idbdkxhhqeuarcqcaweo.supabase.co"
 const SUPABASE_KEY = "sb_publishable_uNWQl8gDfrmrPWS_1uEVcg_gMeqjO2U"
 
 export const sb = createClient(SUPABASE_URL, SUPABASE_KEY)
-export { SUPABASE_URL, SUPABASE_KEY }
